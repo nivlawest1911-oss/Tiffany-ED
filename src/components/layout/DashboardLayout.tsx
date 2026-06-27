2bd936e02ff97ea90dbaafab9ec4c0e9058fcf51
nivlawest1911-oss <nivlawest1911@gmail.com>
2026-06-26 23:01:50 -0500

    feat(design): final sweep - integrate TopNav into DashboardLayout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,12 +1,16 @@
 import { Sidebar } from '@/components/Sidebar';
+import { TopNav } from '@/components/TopNav';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
-      <main className="flex-1">
-        {children}
-      </main>
+      <div className="flex-1 flex flex-col">
+        <TopNav />
+        <main className="flex-1 overflow-auto p-8">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
